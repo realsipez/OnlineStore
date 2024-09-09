3a9f861b1ebf86a82e0809237f4f2e132086a34d
Sina <realsipez@gmail.com>
2024-09-09 16:50:06 +0330

    applying some changes

--- Order/src/main/java/me/sina/order/OrderServer.java
+++ Order/src/main/java/me/sina/order/OrderServer.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = {"me.sina.client.payment"})
+@EnableFeignClients(basePackages = {"me.sina.client.rest.payment"})
 public class OrderServer {
     public static void main(String[] args) {
         SpringApplication.run(OrderServer.class, args);
